0a33d4e851dd345c78125500f564cd3806e33ea3
diegorego514-ship-it <diegorego514@gmail.com>
2025-12-19 17:23:40 -0300

    Add files via upload

---
+++ netrecon.py
@@ -0,0 +1,26 @@
+# ipaddress
+import ipaddress as ipaddress 
+import nmap as nmap
+
+def list_target_ipaddress():
+
+    if str('then print the ipaddress of the target: '):
+        print(ipaddress,  + 'str', 'as', list_target_ipaddress)
+
+def find_target_ip_address():
+
+        def then():
+            if find_target_ip_address():
+                then()
+                print('You have been hacked!\n:')
+        
+            def failed_to_get_target_ipaddress():
+                if failed_to_get_target_ipaddress():
+                    then()
+                    print('ConnectionAbortedError')
+
+        
+            def target_adquired(open_port = 22):
+                (open_port == 22 + target_adquired, list_target_ipaddress and find_target_ip_address)
+                then()
+                print('target_locked_on')
